43cdd71c01747c09bff456821cb7d5aae08b30bc
Ahmet <ahmetalper57doruk@gmail.com>
2021-12-17 23:29:03 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,78 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import reportWebVitals from './reportWebVitals';
-import "./app.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./routers/AppRouter";
+import reportWebVitals from "./reportWebVitals";
+import "./app.css";
 //Redux core
-import {createStore, combineReducers} from "redux"
+import { createStore, combineReducers } from "redux";
 
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
-
-const blogState= [];
+const blogState = [];
 
 //Action creator
-const addBlog = ({title="No Title", description="No description.", dateAdded=0}) => ({
+const addBlog = ({
+  title = "No Title",
+  description = "No description.",
+  dateAdded = 0,
+}) => ({
   type: "ADD_BLOG",
   blog: {
     id: uuid(),
     title: title,
     description: description,
-    dateAdded: dateAdded
-  }
-})
+    dateAdded: dateAdded,
+  },
+});
 
-const blogReducer = (state = blogState, action) =>{
+const blogReducer = (state = blogState, action) => {
   switch (action.type) {
     case "ADD_BLOG":
-      return [...state, action.blog]
-    
-    
-    
+      return [...state, action.blog];
+
     default:
       return state;
   }
-}
+};
 
 
-const authState= {};
 
-const authReducer = (state = authState, action) =>{
+const authState = {};
+
+const authReducer = (state = authState, action) => {
   switch (action.type) {
     default:
       return state;
   }
-}
-
-
+};
 
 const store = createStore(
   // Birden fazla reducer i bir arada store içinde kullanabilmeyi sağlıyor.
   combineReducers({
     blogs: blogReducer,
-    auth: authReducer
+    auth: authReducer,
   })
 );
 
-store.subscribe(() =>{
+store.subscribe(() => {
   console.log(store.getState());
 });
 
-
 //Action
-store.dispatch(addBlog({title: "Hi", description: "cat.", dateAdded: Date.now()}));
+store.dispatch(
+  addBlog({ title: "Hi", description: "cat.", dateAdded: Date.now() })
+);
 
-
-
-
-
-
-
-
-
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
